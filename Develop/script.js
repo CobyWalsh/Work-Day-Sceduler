c91d91a6d49df78b8d2554e310005ca5ee995440
coby walsh <cobywalsh7@yahoo.com>
2023-05-14 12:14:25 -0600

    attempted to add current time and date to webpage

--- Develop/script.js
+++ Develop/script.js
@@ -10,32 +10,46 @@ const toDoForm = document.getElementById("todo-form");
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-Array.from(rows).forEach(row => {
-  let
-    rowIdString = row.id,
-    rowHour;
-  if (rowIdString) {
-    rowHour = parseInt(rowIdString);
-  }
-  if (rowHour) {
-    // Compares row id to current hour and sets color accordingly
-    if (currentHour === rowHour) {
-      setColor(row, "red");
-    } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
-      setColor(row, "green");
-    } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
-      setColor(row, "lightgrey");
-    } else {
-      setColor(row, "white");
+
+const date = Date();
+let day = date.getDate()
+let month = date.getMonth() + 1
+let year = date.getFullYear()
+let time = date.getTime()
+
+let fullDate = '${month}-${day}-${year}-${time}'
+console.log(fulldate);
+
+function upDateSchedule() {
+  Array.from(rows).forEach(row => {
+    let
+      rowIdString = row.id,
+      rowHour;
+    if (rowIdString) {
+      rowHour = parseInt(rowIdString);
     }
-  }
-});
+    if (rowHour) {
+      // Compares row id to current hour and sets color accordingly
+      if (currentHour === rowHour) {
+        setColor(row, "red");
+      } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
+        setColor(row, "green");
+      } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
+        setColor(row, "lightgrey");
+      } else {
+        setColor(row, "white");
+      }
+    }
+  });
+}
 
-let toDoStorage = localStorage.getItem("todo")
-  ? JSON.parse(localStorage.getItem("todo"))
-  : [];
+function addToDO() {
+  let toDoStorage = localStorage.getItem("todo")
+    ? JSON.parse(localStorage.getItem("todo"))
+    : [];
 
- toDoForm.addEventListener("todo-save", (e) => {
+  toDoForm.addEventListener("todo-save", (e) => {
+   console.log("clicked save button")
     e.preventDefault();
     toDoStorage.push(toDoInput.value);
     localStorage.setItem("todo", JSON.stringify(toDoStorage));
@@ -50,17 +64,18 @@ let toDoStorage = localStorage.getItem("todo")
   };
 
   const getNotes = JSON.parse(localStorage.getItem("todo"));
-getNotes.forEach((note) => {
-  listBuilder(note);
-});
+  getNotes.forEach((note) => {
+    listBuilder(note);
+  });
 
-const deleteNote = (btn) => {
-  let el = btn.parentNode;
-  const index = [...el.parentElement.children].indexOf(el);
-  notesStorage.splice(index, 1);
-  localStorage.setItem("todo", JSON.stringify(notesStorage));
-  el.remove();
-};
+  const deleteNote = (btn) => {
+    let el = btn.parentNode;
+    const index = [...el.parentElement.children].indexOf(el);
+    notesStorage.splice(index, 1);
+    localStorage.setItem("todo", JSON.stringify(notesStorage));
+    el.remove();
+  };
+}
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
